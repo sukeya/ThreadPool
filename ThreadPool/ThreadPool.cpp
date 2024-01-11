085f609f63121b8964116c213f77994515d32528
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2024-01-11 14:27:24 +0900

    fix a warning caused std::mutex not providing move.
    This change will make the performance worse because of the dereferences
    of allocated std::mutex and std::condition_variable.
    
    Signed-off-by: Yuya Asano <64895419+sukeya@users.noreply.github.com>

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -1,16 +1,39 @@
 #include "ThreadPool/ThreadPool.hpp"
 
 namespace ThreadPool {
+ThreadPool::ThreadPool()
+    : workers_(),
+      tasks_(),
+      queue_mutex_ptr_(std::make_unique<std::mutex>()),
+      condition_ptr_(std::make_unique<std::condition_variable>()),
+      stop_(false) {}
+
+ThreadPool::ThreadPool(ThreadPool&& thread_pool)
+    : workers_(std::move(thread_pool.workers_)),
+      tasks_(std::move(thread_pool.tasks_)),
+      queue_mutex_ptr_(std::move(queue_mutex_ptr_)),
+      condition_ptr_(std::move(thread_pool.condition_ptr_)),
+      stop_(thread_pool.stop_) {}
+
+ThreadPool& ThreadPool::operator=(ThreadPool&& thread_pool) {
+  workers_         = std::move(thread_pool.workers_);
+  tasks_           = std::move(thread_pool.tasks_);
+  queue_mutex_ptr_ = std::move(thread_pool.queue_mutex_ptr_);
+  condition_ptr_   = std::move(thread_pool.condition_ptr_);
+  stop_            = thread_pool.stop_;
+  return *this;
+}
+
 // the constructor just launches some amount of workers_
-ThreadPool::ThreadPool(size_t threads) : stop_(false) {
-  for (size_t i = 0; i < threads; ++i)
+ThreadPool::ThreadPool(size_t threads_size) : ThreadPool() {
+  for (size_t i = 0; i < threads_size; ++i)
     workers_.emplace_back([this] {
       for (;;) {
         auto task = std::packaged_task<void()>();
 
         {
-          auto lock = std::unique_lock<std::mutex>(queue_mutex_);
-          condition_.wait(lock, [this] { return stop_ || !tasks_.empty(); });
+          auto lock = std::unique_lock<std::mutex>(*queue_mutex_ptr_);
+          condition_ptr_->wait(lock, [this] { return stop_ || !tasks_.empty(); });
           if (stop_ && tasks_.empty()) {
             return;
           }
@@ -26,9 +49,9 @@ ThreadPool::ThreadPool(size_t threads) : stop_(false) {
 // the destructor joins all threads
 ThreadPool::~ThreadPool() {
   {
-    auto lock = std::unique_lock<std::mutex>(queue_mutex_);
+    auto lock = std::unique_lock<std::mutex>(*queue_mutex_ptr_);
     stop_     = true;
-    condition_.notify_all();
+    condition_ptr_->notify_all();
   }
   for (std::thread& worker : workers_) {
     worker.join();
